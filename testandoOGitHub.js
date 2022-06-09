a17de7c9c344268df076f2fbad7f661d6757b52d
Diogo Moreira <you@example.com>
2022-06-09 13:39:20 -0300

    incrementando array

--- testandoOGitHub.js
+++ testandoOGitHub.js
@@ -1,3 +1,8 @@
-ansdckalsc
-asyncasc
-asyncX
+let nome = "Diogo"
+let sobrenome = "Moreira"
+
+function seiLa(nome, sobrenome){
+  return nome + " " + sobrenome
+
+}
+console.log(seiLa("Marco","Langer"))
